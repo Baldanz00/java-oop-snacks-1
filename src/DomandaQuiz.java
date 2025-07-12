d9058784267e80de96bd8db01516f29b69e467a0
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-12 10:40:56 +0200

    snack4

---
+++ src/DomandaQuiz.java
@@ -0,0 +1,23 @@
+public class DomandaQuiz {
+    private String testoDomanda;
+    private String rispostaCorretta;
+    public DomandaQuiz(String testoDomanda, String rispostaCorretta) {
+        this.testoDomanda = testoDomanda;
+        this.rispostaCorretta = rispostaCorretta;
+    }
+    public boolean verificoRisposta(String rispostaUtente) {
+        return rispostaCorretta.equalsIgnoreCase(rispostaUtente.trim());
+
+    }
+
+    public void stampaRisultato(String rispostaUtente) {
+        if(verificaRisposta(rispostaUtente)) {
+            System.out.println("Risposta corretta");
+        } else {
+            System.out.println("Risposta sbagliata");
+        }
+    }
+    public String getTestoDomanda() {
+        return testoDomanda;
+    }
+}
